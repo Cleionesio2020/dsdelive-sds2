86a77aab610376183c61f43bc4ef5151b64243c9
Cleionesio2020 <cleionesio@gmail.com>
2021-01-12 20:49:32 -0300

    revisao maps

--- front-web/src/Api.ts
+++ front-web/src/Api.ts
@@ -4,6 +4,7 @@ import { OrderPlayload } from "./orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+                                
 export function fechProducts(){
     return axios.get(`${API_URL}/products`);
 }
